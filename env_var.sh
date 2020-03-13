ec9a6a0bca2ed2d4c34ba7a8b7b41f22f71a5ce7
jenniferkjin <xjenniferjin@gmail.com>
2020-03-12 22:12:55 -0400

    changed mongodb dockerfile to include backup keyservers, refactored environment variables in docker-compose

---
+++ env_var.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export langAnalyzer="ambar_en"
+export dataPath="/mnt/disk1/db"
+export ambarHostIpAddress="localhost"
+export pathToCrawl="/mnt/disk1/Library"
---
+++ jenn_var.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export langAnalyzer="ambar_en"
+export dataPath="/Users/jennifer.jin/OneDrive - Accenture Federal Services/Downloads/social_media/offline_dxc/ambar_data"
+export ambarHostIpAddress="localhost"
+export pathToCrawl="/Users/jennifer.jin/OneDrive - Accenture Federal Services/Downloads/social_media/offline_dxc/where_is_scarjo copy 2"
